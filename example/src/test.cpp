1f41d2a23f0ca7f9616da183b65bcdbae840a7db
Abhishek Swain <abhiswain.it.2016@gmail.com>
2022-02-15 00:35:31 +0530

    changes to test.cpp

--- example/src/test.cpp
+++ example/src/test.cpp
@@ -1,7 +1,11 @@
 #include "test.h"
 
-int main(){
-    auto x = torch::eye(10);
+int main()
+{
+    auto y = torch::eye(10);
+
+    auto x = torch::sigmoid(torch::randn({10, 10}));
 
     std::cout << x << std::endl;
+    std::cout << y << std::endl;
 }
